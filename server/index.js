caa931208970d402ad125e5d6a7f042ccc90bf01
Ryan <ryan@Ryans-MacBook-Pro.local>
2019-01-25 12:15:43 -0700

    Database complete

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ctrl = require('./controller')
+const massive = require('massive')
 require('dotenv').config()
 
 const app = express()
 app.use(bodyParser.json())
 
+massive(process.env.CONNECTION_STRING)
+.then((db) => {app.set('db', db)
+}).catch(err => console.log (err))
 
 
 
 
-const PORT = 4000
-app.listen(PORT, () => console.log(`Server is listening on ` + PORT))
+const SERVERPORT = process.env.SERVERPORT
+app.listen(SERVERPORT, () => console.log(`Server is listening on ` + SERVERPORT))
